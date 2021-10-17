549b3cbb71d4e6882a026fd4bef36e986e5270b5
Konstantin Zabaznov <konsi.zabaznov@web.de>
2021-10-17 21:47:10 +0200

    Push to GitHub

---
+++ app/controllers/UnoController.scala
@@ -0,0 +1,56 @@
+
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+import UNO.util.{State, gameStatsEvent, instructionEvent}
+
+@Singleton
+class UnoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  val controller = UNO.UnoGame.controller
+
+
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok("OK")
+  }
+
+  def ownText(Text: String) = Action { implicit request: Request[AnyContent] =>
+    Ok(Text)
+  }
+
+
+  def startGame() = Action { implicit request: Request[AnyContent] =>
+    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
+  }
+
+  def setGame(index : Int) = Action { implicit request: Request[AnyContent] =>
+    controller.removeCard(index)
+    Ok(State.handle(gameStatsEvent()))
+  }
+
+  def getGame() = Action { implicit request: Request[AnyContent] =>
+    controller.getCard()
+    Ok(State.handle(gameStatsEvent()))
+  }
+
+  def redoGame() = Action { implicit request: Request[AnyContent] =>
+    controller.redoGet
+    Ok(State.handle(gameStatsEvent()))
+  }
+
+  def undoGame() = Action { implicit request: Request[AnyContent] =>
+    controller.undoGet
+    Ok(State.handle(gameStatsEvent()))
+  }
+
+  def saveGame() = Action { implicit request: Request[AnyContent] =>
+    controller.save
+    Ok(State.handle(gameStatsEvent()))
+  }
+
+  def loadGame() = Action { implicit request: Request[AnyContent] =>
+    controller.load
+    Ok(State.handle(gameStatsEvent()))
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,29 @@
+
+name := """Sudoku in Scala for WebTech"""
+
+version := "1.2"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+resolvers += Resolver.sonatypeRepo("snapshots")
+
+scalaVersion := "2.12.15"
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "com.h2database" % "h2" % "1.4.196"
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "UNO-WebApp.controllers._"
+
+// Adds additional packages into conf/routes
+//play.sbt.routes.RoutesKeys.routesImport += "UNO-WebApp.binders._"
+
+
+//libraryDependencies += guice
+
+//libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
+
+//libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
+
+//libraryDependencies += "com.h2database" % "h2" % "1.4.196"
