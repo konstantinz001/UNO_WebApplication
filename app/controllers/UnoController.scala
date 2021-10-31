225e252ea6c974cb4ca7456efd173fcff4d4621c
Konstantin Zabaznov <konsi.zabaznov@web.de>
2021-10-31 02:43:36 +0200

    CSS

--- app/controllers/UnoController.scala
+++ app/controllers/UnoController.scala
@@ -3,63 +3,14 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
-//import UNO.util.{State, gameStatsEvent, instructionEvent}
-//import UNO.UnoGame
 import UNO.aview.TUI
 import UNO.controller.controllerComponent.controllerInterface
 
 @Singleton
 class UnoController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  //val controller = UNO.UnoGame.controller
   val controller: controllerInterface = UNO.UnoGame.controller
   val tui = new TUI(controller)
 
-//___________________TASK1_______________________________________________________________
-  /*def startGame() = Action { implicit request: Request[AnyContent] =>
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def setGame(index : String) = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("r " + index)
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def getGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("s")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def unoGame(index: String) = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("u " + index)
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def redoGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("redo")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def undoGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("undo")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def saveGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("save")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def loadGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("load")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-
-  def quitGame() = Action { implicit request: Request[AnyContent] =>
-    tui.processInputLine("q")
-    Ok(State.handle(instructionEvent()) + State.handle(gameStatsEvent()))
-  }
-*/
-//_____________________________TASK2_____________________________________________
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
@@ -72,8 +23,12 @@ class UnoController @Inject()(cc: ControllerComponents) extends AbstractControll
     Ok(views.html.tui(tui))
   }
 
-  def instructionExecute(input: String, index: String) = Action {
-    tui.processInputLine(input + " " + index)
+  def instructionExecute(input: String, index: String, unoIndex:String) = Action {
+    if(unoIndex == "") {
+      tui.processInputLine(input + " " + index)
+    } else {
+      tui.processInputLine(input + " " + unoIndex)
+    }
     Ok(views.html.tui(tui))
   }
 }
